12ddabd5dd7e65aeef6255a8d440da9147233dac
Leigh Butler <24991566+ctulb@users.noreply.github.com>
2024-05-22 08:44:26 +0100

    complete login form

--- tests/login_test.ts
+++ tests/login_test.ts
@@ -5,7 +5,7 @@ import {
   assertStringIncludes,
 } from "https://deno.land/std@0.224.0/assert/mod.ts";
 
-Deno.test("login endpoint", async (t) => {
+Deno.test("login endpoint GET", async (t) => {
   await t.step("exists", async () => {
     const res = await app.request("/login");
 
@@ -24,4 +24,22 @@ Deno.test("login endpoint", async (t) => {
 
     assertStringIncludes(content, "form");
   });
+  await t.step(
+    "contains an input to collect an email address for login",
+    async () => {
+      const res = await app.request("/login");
+      const content = await res.text();
+
+      assertStringIncludes(content, 'input type="email"');
+    },
+  );
+  await t.step(
+    "contains a submit button to trigger login attempt",
+    async () => {
+      const res = await app.request("/login");
+      const content = await res.text();
+
+      assertStringIncludes(content, 'button type="submit"');
+    },
+  );
 });
